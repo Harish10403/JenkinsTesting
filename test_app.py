f908734205c6363a9dae4ba1707d7dc190424b13
Harish10403 <rmharish2017@gmail.com>
2024-11-20 12:53:07 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,10 +2,8 @@ from app import add, subtract
 
 def test_add():
     assert add(3, 5) == 8
-    print(3+5)
     assert add(-1, 1) == 0
 
 def test_subtract():
     assert subtract(10, 5) == 5
-    print(10-5)
     assert subtract(0, 5) == -5
